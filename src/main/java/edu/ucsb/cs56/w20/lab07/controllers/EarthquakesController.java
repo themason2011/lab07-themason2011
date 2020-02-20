3a00f078da858c6156318282e0e33a0931d7aec4
themason2011 <mcorey@umail.ucsb.edu>
2020-02-19 16:06:42 -0800

    mc - added form for search at url /earthquakes/search

---
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/EarthquakesController.java
@@ -0,0 +1,18 @@
+package edu.ucsb.cs56.w20.lab07.controllers;
+
+import edu.ucsb.cs56.w20.lab07.formbeans.EqSearch;
+import org.springframework.stereotype.Controller;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EarthquakesController {
+	@GetMapping("/earthquakes/search")
+    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        return "earthquakes/search";
+    }
+   
+}
